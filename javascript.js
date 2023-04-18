6a01f2e188dfd549d396aa23d7fc47fdcf8bbb5a
Falllstreak <23.anomaly@gmail.com>
2023-04-18 14:37:16 -0700

    Added the squared calculation to the first button, and edited background color.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,35 @@
-var buttons
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    let myArray = [2, 3, 4];
+
+    document.getElementById("square").addEventListener("click", function () {
+        let x = myArray.map(mySquare);
+        //let x = myArray.map(x => x.Square);
+        document.getElementById("output1").value = x;
+    });
+    
+    document.getElementById("cubed").addEventListener("click", function () {
+        let x = myArray.map(myCubed);
+        
+
+        document.getElementById("output2").value = x;
+    });
+    
+    document.getElementById("fourth").addEventListener("click", function () {
+        let x = myArray.map(fourthFunction)
+        document.getElementById("output3").value = x;
+    });
+});
+
+function mySquare(oneItem) {
+    return Math.pow(oneItem, 2);
+}
+
+function myCubed(oneItem) {
+    return Math.pow(oneItem, 3)();
+}
+
+function fourthFunction(oneItem) {
+    charArray = Array.from(oneItem)
+    return charArray[0];
+}
